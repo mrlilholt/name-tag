48b3e6fed8534a2f3393e16aac99d61f5e481bf9
mrlilholt <63080594+mrlilholt@users.noreply.github.com>
2023-02-16 10:45:32 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-basic.show_string("My name is: ")
-basic.show_string("Mr. Lilholt")
-basic.show_string("My age is: ")
-basic.show_number(36)
-basic.show_string("Favorite color: ")
-basic.show_string("Blue")
+def on_forever():
+    basic.show_string("Mr. Lilholt")
+basic.forever(on_forever)
+
 
